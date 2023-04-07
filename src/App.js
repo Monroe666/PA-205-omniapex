a6cbacfc6100d42e4c5a3b6ce5e0da34b9b13ddb
Wenjie Chen <s3637291@student.rmit.edu.au>
2023-04-07 13:12:05 +1000

    Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navibar from "./fragments/Navibar";
+import Music from "./fragments/Music";
+import TopNav from "./fragments/TopNav";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
 
 function App() {
   return (
     <div>
+      <Router>
+        <Navibar />
+        <Music />
+        <TopNav />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/video" element={<Video />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
