dc02134991a7409d3dd297984a46616d549e059d
Wenjie Chen <s3637291@student.rmit.edu.au>
2023-05-04 23:34:11 +1000

    Repair conflict

--- src/fragments/TopNav.js
+++ src/fragments/TopNav.js
@@ -11,12 +11,20 @@ import useSearch from "./useSearch";
 
 const TopNav = () => {
   const { searchResults, handleSearch, query } = useSearch();
+
   const [isFocused, setIsFocused] = useState(false);
-  const loggedin = localStorage.getItem("loggedin") || false;
+
+  const [loggedin, setLoggedin] =
+    useState(localStorage.getItem("loggedin") || false);
+
   const navigate = useNavigate();
 
+  const [isNotifiOpen, setNotifiOpen] = useState(false);
+
   const handleLogout = () => {
+    localStorage.removeItem("account");
     localStorage.removeItem("loggedin");
+    setLoggedin(false);
     navigate("/sign");
   };
 
@@ -32,6 +40,7 @@ const TopNav = () => {
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
       />
+
       {loggedin ? (
         <Link to="/userprofile">
           <FaRegUser className="user" />
@@ -41,25 +50,37 @@ const TopNav = () => {
           <FaRegUser className="user" />
         </Link>
       )}
-      <IoMdNotificationsOutline className="notice" />
+
+      <IoMdNotificationsOutline className="notice" onClick={() => setNotifiOpen(!isNotifiOpen)} />
+      <div className={isNotifiOpen ? "boxOpen" : "notifi_box"}>
+        <h2 className="notifi_title">Notification <span className="notifi_amount">1</span></h2>
+        <div className="notifi_item">
+          <div className="text">
+            <h4>A</h4>
+            <p>Hello!</p>
+          </div>
+        </div>
+      </div>
+
       <Link to="/messaging">
         <BiMessageRounded className="chat" />
       </Link>
+
       <BiLogOut className="logout" onClick={handleLogout} />
 
       {isFocused && (
-  <ul className="search-results">
-    {searchResults.length > 0 ? (
-      searchResults.map((result) => (
-        <li key={result.id}>{result.title}</li>
-      ))
-    ) : query.length === 0 ? (
-      <li>None</li>
-    ) : (
-      <li>No results found</li>
-    )}
-  </ul>
-)}
+        <ul className="search-results">
+          {searchResults.length > 0 ? (
+            searchResults.map((result) => (
+              <li key={result.id}>{result.title}</li>
+            ))
+          ) : query.length === 0 ? (
+            <li>None</li>
+          ) : (
+            <li>No results found</li>
+          )}
+        </ul>
+      )}
     </div>
   );
 };
