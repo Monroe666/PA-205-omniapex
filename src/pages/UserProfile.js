af7f2fffcb8344198a1cfc8cdfcfba727e5f063d
Wenjie Chen <s3637291@student.rmit.edu.au>
2023-05-05 20:42:05 +1000

    Improve profile page front end

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,6 +1,4 @@
 import "../styles/UserProfile.css";
-import Like from "../assets/like.png";
-import Create from "../assets/create.png";
 import Music from "../fragments/Music"
 import ProfilePic from "../assets/profilepic.png";
 import { useEffect, useState } from "react";
@@ -21,17 +19,31 @@ const UserProfile = () => {
     return (
         <div>
             <div className="home">
-                <h1 className="userprofile_title">User Profile</h1>
-                <img className="profilepic" src={ProfilePic}></img>
+                <img className="profile_img" src={ProfilePic}></img>
                 {user.map(d => (<div>
-                    <h1 className="profile_account">Account: {d.account}</h1>
-                    <p className="profile_name">Name: {d.username}</p>
-                    <p className="profile_type">Type: {d.usertype}</p>
+                    <h1 className="profile_name">{d.username}</h1>
+                    <h3 className="profile_account">Email: {d.account}</h3>
+                    <p className="profile_type">User type: {d.usertype}</p>
                 </div>
                 ))}
-                <h1 className="list_title">Playlists</h1>
-                <button className="like_list"><img className="like_list_img" src={Like}></img>Liked Songs</button>
-                <button className="create_list"><img className="create_list_img" src={Create}></img>Create List</button>
+
+                <h4 className="profile_num_1">888k</h4>
+                <p className="profile_name_1">Follower</p>
+
+                <h4 className="profile_num_2">88</h4>
+                <p className="profile_name_2">Composition</p>
+
+                <h4 className="profile_num_3">8m</h4>
+                <p className="profile_name_3">Viewer</p>
+
+                <button className="profile_follow">Follow</button>
+                <button className="profile_more">+ View more</button>
+
+                <h1 className="bio_title">ABOUT</h1>
+                <p className="profile_bio">Emmelie is a traditional book-worm and has always been from a young age.
+                    She is a housekeeper mom with two kids and she has a lot of time to read and relax.
+                    Emmelie tends to casually browse books in a bookstore but she usually has a hard time finding the
+                    right one and spends a lot of time browsing.</p>
             </div>
             <Music />
         </div>
