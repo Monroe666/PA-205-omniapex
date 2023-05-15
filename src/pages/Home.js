0cdb568891018ddaacd9160881ad9ca9884f2b7c
NandiniPandya15 <110213107+NandiniPandya15@users.noreply.github.com>
2023-05-15 16:30:30 +1000

    Link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import "../styles/Home.css";
+import "../pages/Albums.js"
 import Like from "../assets/like.png";
 import Create from "../assets/create.png";
 import Music from "../fragments/Music"
@@ -9,7 +10,7 @@ const Home = () => {
             <div className="home">
                 <h1 className="home_title">Streaming music now!</h1>
                 <button className="artist">Artist</button>
-                <button className="album">Album</button>
+                <button className="album" a href="Albums.js">Album</button>
                 <button className="genre">Genre</button>
                 <h1 className="list_title">Playlists</h1>
                 <button className="like_list"><img className="like_list_img" src={Like}></img>Liked Songs</button>
