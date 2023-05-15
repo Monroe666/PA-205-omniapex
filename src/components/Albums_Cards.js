b6c3d6d44f1b871738dc919f9b87efe774360311
NandiniPandya15 <110213107+NandiniPandya15@users.noreply.github.com>
2023-05-15 16:22:55 +1000

    Albums_Cards JavaScript

---
+++ src/components/Albums_Cards.js
@@ -0,0 +1,16 @@
+import "../styles/Albums_Cards.css";
+
+const Albums_Cards= () => {
+    return (
+        <div className="albums_card">
+            <img className="albums_cover" src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Compact_disc_album.jpg"></img>
+            <div className="albums_detail">
+                <h1 className="albums_title">This is the author name</h1>
+            </div>
+            <p className="albums_au_name">This is a Title </p>
+            <p className="albums_au_name">235K views - 1 year ago</p>
+        </div>
+    )
+}
+
+export default Albums_Cards;
