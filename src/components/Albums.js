9583440f60352dc7d41859c7a73966fd4f4c21a2
NandiniPandya15 <110213107+NandiniPandya15@users.noreply.github.com>
2023-05-15 16:18:07 +1000

    Album JavaScript

---
+++ src/components/Albums.js
@@ -0,0 +1,19 @@
+import "../styles/Albums.css";
+import Albums_Cards from "../components/Albums_Cards";
+
+const Albums = () => {
+    return (
+            <div className="album">
+                <Albums_Cards />
+                <Albums_Cards/>
+                <Albums_Cards />
+                <Albums_Cards/>
+                <Albums_Cards/>
+                <VAlbums_Cards/>
+                <Albums_Cards/>
+                <Albums_Cards/>
+            </div>        
+    )
+}
+
+export default Albums;
