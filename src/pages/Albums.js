4f12822c283b1c8992a8881d7e2f8f56a5670ae3
Nandini Pandya <s3957496@student.rmit.edu.au>
2023-05-17 19:02:51 +1000

    change

--- src/pages/Albums.js
+++ src/pages/Albums.js
@@ -4,14 +4,14 @@ import Albums_Cards from "../components/Albums_Cards";
 const Albums = () => {
     return (
             <div className="Albums">
-                <Albums_Cards />
-                <Albums_Cards/>
-                <Albums_Cards />
+                <Albums_Cards /> 
                 <Albums_Cards/>
                 <Albums_Cards/>
                 <Albums_Cards/>
+                <Albums_Cards /> 
                 <Albums_Cards/>
                 <Albums_Cards/>
+                <Albums_Cards /> 
             </div>        
     )
 }
